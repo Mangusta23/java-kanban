f87c0d0f651bd00f014216464d7505e00f059218
Mangusta23 <140246717+Mangusta23@users.noreply.github.com>
2023-09-06 03:23:06 +0300

    Add files via upload

--- src/Manager/Managers.java
+++ src/Manager/Managers.java
@@ -1,11 +1,20 @@
 package Manager;
+import Model.*;
 
 import Manager.InMemoryTaskManager;
 import Manager.TaskManager;
 
-public class Managers {
+public class Managers<T> {
+    public T element;
+    public Managers(T element){
+        this.element = element;
+    }
     public TaskManager getDefault(){
-        TaskManager taskManager = new InMemoryTaskManager();
-        return taskManager;
+        if (element instanceof Task || element instanceof SubTask || element instanceof Epic) {
+            InMemoryTaskManager taskManager = new InMemoryTaskManager();
+            return taskManager;
+        }else{
+            return null;
+        }
     };
 }
